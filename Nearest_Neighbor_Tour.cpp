f6360f5453ce4734e826438ec7869410ceb5e6e0
Andrew <airbag15@gmail.com>
2018-06-28 16:35:48 -0400

    first commit

---
+++ Nearest_Neighbor_Tour.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Pick and visit an intial point p0
+	// initialize points p, p0, and p1
+	int p0 = 0;
+	int p = p0;
+	int i = 0;
+	
+
+	// While there are still unvisited points
+	while (p < p0++)
+	{
+		i = i + 1;
+		int pi = 0;
+		int pi_minus_one = pi - 1;
+		cout << pi_minus_one;
+		return p0;
+	}
+	system("pause");
+	return 0;
+	// Let pi be the closest unvisited point to pi-1, visit pi
+	
+}
